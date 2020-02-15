bae2dc130a27768778507b78ec6bac6b94ab6dfa
catherine <itscatherinehlee@gmail.com>
2020-02-15 02:41:01 -0500

    navigation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import './index.css';
+//browserrouter should be used when you have a server that will handle dyanimc requestis
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+//because router componenets only expect to receive a single child element, create <App> 
+//componenet that renders the rest of your app
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
